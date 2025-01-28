ad66a6d2122de303176f2ff92a72b30a3b9b8dc6
Obada Tabbaa <obatabba@gmail.com>
2025-01-28 22:56:54 +0300

    Automate deleting orphan uploaded files.

--- employment/signals.py
+++ employment/signals.py
@@ -1,8 +1,9 @@
+import os
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete, post_delete
 from django.dispatch import receiver
 
-from .models import Employee, Employer
+from .models import Application, Employee, Employer, User
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -12,3 +13,9 @@ def create_profile(sender, instance, created, **kwargs):
             Employee.objects.create(user=instance)
         elif instance.account_type == 'employer':
             Employer.objects.create(user=instance)
+
+
+@receiver(post_delete, sender=Application)
+def delete_uploads(sender, instance: Application, **kwargs):
+    if instance.resume:
+        os.remove(os.path.join(settings.MEDIA_ROOT, instance.resume.name))
