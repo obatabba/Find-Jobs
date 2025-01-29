dced93a0b0d7e5cbd5b13c4a5c905115a67b6437
Obada Tabbaa <obatabba@gmail.com>
2025-01-29 18:19:45 +0300

    Add field "expertise" to employee.

--- employment/models.py
+++ employment/models.py
@@ -81,6 +81,7 @@ class Job(models.Model):
 
 class Employee(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    expertise = models.CharField(max_length=255)
     about = models.TextField(null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True, blank=True)
