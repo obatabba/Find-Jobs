e5dce508495be5b5206ee8c89956c1053125e184
Obada Tabbaa <obatabba@gmail.com>
2025-03-07 03:46:21 +0300

    ALLOWED_HOSTS now must be separated by a comma "," instead of a space.

--- FindJobs/settings/prod.py
+++ FindJobs/settings/prod.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ['DATABASE_URL'])
